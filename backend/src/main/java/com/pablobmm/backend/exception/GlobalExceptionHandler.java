f72688dcf9d69a8a83b7b097243ce4e896ffff34
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-07-10 18:25:35 -0300

    feat: adiciona tratamento global de erros de validação

---
+++ backend/src/main/java/com/pablobmm/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.pablobmm.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> tratarValidacao(MethodArgumentNotValidException ex){
+
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(erro -> {
+            erros.put(
+                    erro.getField(),
+                    erro.getDefaultMessage()
+            );
+        });
+
+        return erros;
+    }
+
+}
