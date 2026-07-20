114239f02498979056715b6fd7f8726d30f51fa5
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-07-20 17:51:13 -0300

    feat: cria entidade profissional

---
+++ backend/src/main/java/com/pablobmm/backend/entity/Profissional.java
@@ -0,0 +1,26 @@
+package com.pablobmm.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profissionais")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profissional {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String telefone;
+
+    private String especialidade;
+    
+}
